7e7ae8e0ee2b9f8fe34490fddf5d77083c446030
BieFeNg <feng.bie@myt11.com>
2019-07-09 20:36:07 +0800

    common commit

--- service-1/src/main/java/com/example/service1/asm/Printer.java
+++
@@ -1,8 +0,0 @@
-package com.example.service1.asm;
-/*
- *@Author BieFeNg
- *@Date 2019/7/1 18:03
- *@DESC
- */
-public interface Printer {
-}
--- service-1/src/main/java/com/example/service1/controller/TestController.java
+++ service-1/src/main/java/com/example/service1/controller/TestController.java
@@ -1,15 +1,8 @@
 package com.example.service1.controller;
 
 
-import com.alibaba.fastjson.JSON;
-import com.example.service1.asm.PrintCommUtil;
-import com.example.service1.asm.Printer;
 import com.example.service1.common.model.PageVo;
 import com.example.service1.service.TestService;
-
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,34 +10,12 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Method;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.objectweb.asm.Opcodes.V1_8;
 
 @RestController
 @CrossOrigin(allowCredentials = "true")
 @RequestMapping("/test")
 public class TestController {
-    private static final Map<String, Method> methodMap = new HashMap<>();
-
-    static {
-
-        Class<?> clazz = null;
-        try {
-            clazz = Class.forName("com.example.service1.asm.PrintCommUtil");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        Method[] declaredMethods = clazz.getDeclaredMethods();
-        for (Method m : declaredMethods) {
-            String name = m.getName();
-            methodMap.put(name, m);
-        }
-    }
 
     @Autowired
     private TestService service;
@@ -80,20 +51,5 @@ public class TestController {
         HttpStatus status = HttpStatus.OK;
         Object data = null;
         return new ResponseEntity(data, status);
-    }
-
-    @PostMapping("/template/add")
-    public ResponseEntity generateTemplate(String templateStr, Map<String, String> printData) throws ClassNotFoundException {
-        List<Map<String, String>> units = JSON.parseObject(templateStr, List.class);
-        for (Map<String, String> unit : units) {
-            String type = unit.get("type");
-            switch (type){
-                case "text":
-
-            }
-
-        }
-        return new ResponseEntity(null, HttpStatus.OK);
-
     }
 }
